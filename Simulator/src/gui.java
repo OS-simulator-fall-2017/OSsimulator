78b3d0418343f649b8713f41e779c8315097022c
mikelewis405 <mikelewis405@gmail.com>
2017-11-22 22:51:08 -0500

    Gui setup Stage

--- Simulator/src/gui.java
+++ Simulator/src/gui.java
@@ -1,18 +1,27 @@
-
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class gui extends Application {
-    public void start(Stage stage) {
-        Circle circ = new Circle(40, 40, 30);
-        Group root = new Group(circ);
-        Scene scene = new Scene(root, 400, 300);
 
-        stage.setTitle("My JavaFX Application");
-        stage.setScene(scene);
-        stage.show();
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
     }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("OS 312");
+        button = new Button();
+        button.setText("Test");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
