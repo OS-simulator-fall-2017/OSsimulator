27fe3fbaf7e9fc3e5e023bf37cd490f7ffcedac6
mikelewis405 <mikelewis405@gmail.com>
2017-11-23 20:11:06 -0500

    Command Line input / New Jframe

--- Simulator/src/gui.java
+++ Simulator/src/gui.java
@@ -1,12 +1,25 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class gui extends Application {
 
     Button button;
+    private BorderPane layout;
+    private TextField CommandInput;
+    private HBox controls;
+    private HBox Input;
+    static protected TextArea textArea;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,13 +28,31 @@ public class gui extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("OS 312");
+        
         button = new Button();
-        button.setText("Test");
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        button.setText("Click Me");
+        StackPane center = new StackPane();
+        center.getChildren().add(button);
+        
+        button.setOnAction(e -> System.out.println("Test"));
+        
+        CommandInput = new TextField();
+      
+        
+        HBox Input = new HBox();
+        Input.getChildren().add(CommandInput);
+        
+        layout = new BorderPane();
+        layout.setTop(center);
+        layout.setBottom(Input);
+       
         Scene scene = new Scene(layout, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+    
+   
+   
+    
+    
 }
