d07c34537db9d6f8c7d078a244afd1b9b4fe13d4
mikelewis405 <mikelewis405@gmail.com>
2017-11-25 14:52:00 -0500

    Boolean values for commands

--- Simulator/src/gui.java
+++ Simulator/src/gui.java
@@ -1,3 +1,4 @@
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -5,8 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -17,10 +16,8 @@ public class gui extends Application {
     Button button;
     private BorderPane layout;
     private TextField CommandInput;
-    private HBox controls;
-    private HBox Input;
     static protected TextArea textArea;
-
+   
     public static void main(String[] args) {
         launch(args);
     }
@@ -34,10 +31,10 @@ public class gui extends Application {
         StackPane center = new StackPane();
         center.getChildren().add(button);
         
-        button.setOnAction(e -> System.out.println("Test"));
+        button.setOnAction(e -> Commands(CommandInput) );
         
         CommandInput = new TextField();
-      
+        
         
         HBox Input = new HBox();
         Input.getChildren().add(CommandInput);
@@ -49,6 +46,23 @@ public class gui extends Application {
         Scene scene = new Scene(layout, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    private boolean Commands(TextField input) {
+    		 if(input.getText().equals("proc"))
+    	         return true;
+    		 if(input.getText().equals("mem"))
+    			 return true;
+    		 if(input.getText().equals("load"))
+    			 return true;    
+    		 if(input.getText().equals("exe"))
+    			 return true;
+    		 if(input.getText().equals("reset"))
+    			 return true;
+    		 if(input.getText().equals("exit"))
+    			 return true;
+    	System.out.println("Error: not a proper command");
+    	return false;
     }
     
    
