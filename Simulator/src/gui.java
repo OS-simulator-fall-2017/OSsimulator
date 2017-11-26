b7c9d0c57b5b6307b084d93ae6fa8cf858303bad
mikelewis405 <mikelewis405@gmail.com>
2017-11-25 20:27:47 -0500

    Dummy Ready Table

--- Simulator/src/gui.java
+++ Simulator/src/gui.java
@@ -1,15 +1,22 @@
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javax.swing.JTable;
 
 public class gui extends Application {
 
@@ -17,6 +24,8 @@ public class gui extends Application {
     private BorderPane layout;
     private TextField CommandInput;
     static protected TextArea textArea;
+    
+    TableView<testTable> table;
    
     public static void main(String[] args) {
         launch(args);
@@ -26,19 +35,35 @@ public class gui extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("OS 312");
         
+      //Name column
+        TableColumn<testTable, String> nameColumn = new TableColumn<>("Job");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        TableColumn<testTable, Double> arrivalColumn = new TableColumn<>("Arrival Time");
+        arrivalColumn.setMinWidth(75);
+        arrivalColumn.setCellValueFactory(new PropertyValueFactory<>("arrival"));
+        
+        TableColumn<testTable, String> runColumn = new TableColumn<>("Run Time");
+        runColumn.setMinWidth(75);
+        runColumn.setCellValueFactory(new PropertyValueFactory<>("run"));
+        
+        table = new TableView<>();
+        table.setItems(getTestTable());
+        table.getColumns().addAll(nameColumn, arrivalColumn, runColumn);
+        
         button = new Button();
         button.setText("Click Me");
-        StackPane center = new StackPane();
-        center.getChildren().add(button);
+        VBox center = new VBox();
+        center.getChildren().addAll(button, table);
         
         button.setOnAction(e -> Commands(CommandInput) );
         
         CommandInput = new TextField();
-        
-        
+          
         HBox Input = new HBox();
         Input.getChildren().add(CommandInput);
-        
+                 
         layout = new BorderPane();
         layout.setTop(center);
         layout.setBottom(Input);
@@ -46,6 +71,16 @@ public class gui extends Application {
         Scene scene = new Scene(layout, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    public ObservableList<testTable> getTestTable(){
+        ObservableList<testTable> jobs = FXCollections.observableArrayList();
+       jobs.add(new testTable("text", 859.00, 20));
+        jobs.add(new testTable("test", 2.49, 198));
+        jobs.add(new testTable("idk", 99.00, 74));
+        jobs.add(new testTable("paintballs", 19.99, 12));
+        jobs.add(new testTable("Wrecked", 1.49, 856));
+        return jobs;
     }
     
     private boolean Commands(TextField input) {
@@ -64,9 +99,6 @@ public class gui extends Application {
     	System.out.println("Error: not a proper command");
     	return false;
     }
-    
-   
-   
     
     
 }
---
+++ Simulator/src/testTable.java
@@ -0,0 +1,44 @@
+
+public class testTable {
+
+    private String name;
+    private double arrival;
+    private int run;
+
+    public testTable(){
+        this.name = "";
+        this.arrival = 0;
+        this.run = 0;
+    }
+
+    public testTable(String name, double arrival, int run){
+        this.name = name;
+        this.arrival = arrival;
+        this.run = run;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(double arrival) {
+        this.arrival = arrival;
+    }
+
+    public int getRun() {
+        return run;
+    }
+
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+}
